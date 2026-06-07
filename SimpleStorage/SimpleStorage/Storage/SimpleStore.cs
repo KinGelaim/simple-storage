89ff99b682403854c744c32a1094c7de2cde2c0e
KinGelaim <89123232156@mail.ru>
2026-06-07 12:56:57 +0500

    Added thread safety to storage

--- SimpleStorage/SimpleStorage/Storage/SimpleStore.cs
+++ SimpleStorage/SimpleStorage/Storage/SimpleStore.cs
@@ -3,16 +3,28 @@ namespace SimpleStorage.Storage;
 /// <summary>
 /// Базовое хранилище
 /// </summary>
-internal sealed class SimpleStore
+internal sealed class SimpleStore : IDisposable
 {
     private readonly Dictionary<string, byte[]> _data = [];
+    private readonly ReaderWriterLockSlim _lock = new();
 
     /// <summary>
     /// Добавление или обновление значения по ключу
     /// </summary>
     /// <param name="key">Ключ</param>
     /// <param name="value">Значение</param>
-    public void Set(string key, byte[] value) => _data[key] = value;
+    public void Set(string key, byte[] value)
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            _data[key] = value;
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
 
     /// <summary>
     /// Возвращает значение по ключу
@@ -21,13 +33,34 @@ internal sealed class SimpleStore
     /// <returns>Значение по ключу или null, если ключ не найден</returns>
     public byte[]? Get(string key)
     {
-        _data.TryGetValue(key, out var value);
-        return value;
+        _lock.EnterReadLock();
+        try
+        {
+            _data.TryGetValue(key, out var value);
+            return value;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
     }
 
     /// <summary>
     /// Удаляет ключ и значение
     /// </summary>
     /// <param name="key">Ключ</param>
-    public void Delete(string key) => _data.Remove(key);
+    public void Delete(string key)
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            _data.Remove(key);
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+
+    public void Dispose() => _lock.Dispose();
 }
