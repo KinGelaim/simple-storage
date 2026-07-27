bb204e1fa9c6b82692047378be8b02afab0eaaae
KinGelaim <89123232156@mail.ru>
2026-07-27 14:41:22 +0500

    13-add-serialization-source-generator Added a project for generators

---
+++ SimpleStorage/SimpleStorage.Generators/BinarySerializerGenerator.cs
@@ -0,0 +1,8 @@
+using Microsoft.CodeAnalysis;
+
+namespace SimpleStorage.Generators;
+
+public sealed class BinarySerializerGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context) => throw new NotImplementedException();
+}
